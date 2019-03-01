4bf3446cf8151b4d6b9ca72d180cce6d63499585
guillaumeNervo <47926074+guillaumeNervo@users.noreply.github.com>
2019-03-01 11:10:41 +0100

    Add files via upload

--- clusterGeo.py
+++ clusterGeo.py
@@ -5,12 +5,16 @@ Created on Sat Feb 23 17:36:14 2019
 @author: guill
 """
 
-from sklearn.cluster import MeanShift
+from sklearn.cluster import KMeans,MeanShift
 import numpy as np
 import ast
+import time
+import matplotlib.pyplot as plt
+import pandas as pd
 
 #train=data_train[data_train.MISSING_DATA!=True].sample(frac=1)
-train=data_train.iloc[:100000][data_train.MISSING_DATA!=True].sample(frac=1)
+data_train=pd.read_csv('C:\\Users\\guill\\Documents\\Cours\\Polytechnique\\3A\\MAP583 Apprentisage Profond\\Projet\\data\\train.csv')
+train=data_train.iloc[:200000][data_train.MISSING_DATA!=True].sample(frac=1)
 
 #dans arrive y a la destination et chemin c'est le debut de la trajectoire (j'ai prit 10 points)
 train_arrive=np.zeros(shape=(len(train),2))
@@ -24,8 +28,18 @@ for i in range(len(train)):
 deleteTrain=np.asarray(np.where(train_arrive[:,0]!=0)).reshape(-1)
 train_arrive=train_arrive[deleteTrain]
 
-clustering = MeanShift().fit(train_arrive)
-clustering.labels_
+print("debut")
+'''
+start=time.time()
+clustering = KMeans(n_clusters=1000).fit(train_arrive)
+end=time.time()
+print(end-start)
+'''
+start=time.time()
+clustering = MeanShift(bandwidth=1000).fit(train_arrive)
+end=time.time()
+print(end-start)
+
 
 from joblib import dump, load
-dump(clustering, 'filename.joblib') 
+dump(clustering, 'clusterDestinationMS.joblib') 
--- premierRN.py
+++ premierRN.py
@@ -5,6 +5,10 @@ Created on Mon Feb 18 18:02:26 2019
 @author: guill
 """
 
+'''
+import json
+import zipfile
+from joblib import dump, load
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -12,52 +16,46 @@ import torch.nn as nn
 import torch.nn.functional as F
 from sklearn import preprocessing
 import pandas as pd
-import ast
+from datetime import datetime
 from operator import itemgetter
 import imp   
+
+import sys
+sys.path.insert(0, 'C:\\Users\\guill\\Documents\\Cours\\Polytechnique\\3A\\MAP583 Apprentisage Profond\\Projet\\')
 import torch_utils; imp.reload(torch_utils)
 #from Loss_Function import Haversine_Loss
 from torch_utils import gpu, minibatch, shuffle , Haversine_Loss
 
 
-data_train=pd.read_csv('C:\\Users\\guill\\Documents\\Cours\\Polytechnique\\3A\\MAP583 Apprentisage Profond\\Projet\\data\\train.csv')
-data_test=pd.read_csv('C:\\Users\\guill\\Documents\\Cours\\Polytechnique\\3A\\MAP583 Apprentisage Profond\\Projet\\data\\test.csv')
+data_train=pd.read_csv('C:\\Users\\guill\\Documents\\Cours\\Polytechnique\\3A\\MAP583 Apprentisage Profond\\Projet\\data\\train.csv',converters={'POLYLINE': lambda x: json.loads(x)})
+#data_test=pd.read_csv('C:\\Users\\guill\\Documents\\Cours\\Polytechnique\\3A\\MAP583 Apprentisage Profond\\Projet\\data\\test.csv',converters={'POLYLINE': lambda x: json.loads(x)})
 
 #j'ai pris un petit nombre juste pour le faire tourner et debuguer
-littleTrain=data_train.iloc[:10000][data_train.MISSING_DATA!=True].sample(frac=1)
+littleTrain=data_train.iloc[:200000][data_train.MISSING_DATA!=True].sample(frac=1)
 littleTest=data_train.iloc[-1000:][data_train.MISSING_DATA!=True].sample(frac=1)
 
-#dans arrive y a la destination et chemin c'est le debut de la trajectoire (j'ai prit 10 points)
-train_arrive=np.zeros(shape=(len(littleTrain),2))
-test_arrive=np.zeros(shape=(len(littleTest),2))
-train_chemin=[0 for i in range(len(littleTrain))]
-test_chemin=[0 for i in range(len(littleTest))]
 
-#je remplis les array et je me fais pas chier si y a pas assez de points je les compte pas pour l'instant
-for i in range(len(littleTest)):
-    tr=np.asarray(ast.literal_eval(littleTrain['POLYLINE'].iloc[i])).flatten()
-    te=np.asarray(ast.literal_eval(littleTest['POLYLINE'].iloc[i])).flatten()
-    if(len(tr)>20):
-        train_arrive[i]=tr[-2:]
-        train_chemin[i]=tr[:20]
-    if(len(te)>20):
-        test_chemin[i]=te[:20]
-        test_arrive[i]=te[-2:]
-    
-    
-for i in range(len(littleTest),len(littleTrain)):
-    tr=np.asarray(ast.literal_eval(littleTrain['POLYLINE'].iloc[i])).flatten()
-    if(len(tr)>10):
-        train_chemin[i]=tr[:20]
-        train_arrive[i]=tr[-2:]
+#je télécharge le clustering fait dans ClusterGeo
+clustering=load('C:\\Users\\guill\\Documents\\Cours\\Polytechnique\\3A\\MAP583 Apprentisage Profond\\Projet\\clusterDestination.joblib')
+
+
+#dans arrive y a la destination et chemin c'est le debut de la trajectoire (j'ai prit 10 points)
+train_arrive = np.array([ np.asarray(p[-1]) if len(p)>10 else [0,0] for p in littleTrain['POLYLINE']])
+train_chemin = np.asarray([ np.asarray(p[:10]).flatten() if len(p)>10 else np.zeros(20) for p in littleTrain['POLYLINE']])
+test_arrive = np.array([ np.asarray(p[-1]) if len(p)>10 else [0,0] for p in littleTest['POLYLINE']])
+test_chemin = np.asarray([ np.asarray(p[:10]).flatten() if len(p)>10 else np.zeros(20) for p in littleTest['POLYLINE']])
+
 
 #juste j'enlève les trajectoires non sélectionnées
-keepTrain=np.asarray(np.where(train_arrive[:,0]!=0)).reshape(-1)
-keepTest=np.asarray(np.where(test_arrive[:,0]!=0)).reshape(-1)
+keepTrain=np.asarray(np.where(train_chemin[:,0]!=0)).reshape(-1)
+keepTest=np.asarray(np.where(test_chemin[:,0]!=0)).reshape(-1)
 train_arrive=train_arrive[keepTrain]
-train_chemin=itemgetter(*keepTrain)(train_chemin)
+train_chemin=train_chemin[keepTrain]
 test_arrive=test_arrive[keepTest]
-test_chemin=itemgetter(*keepTest)(test_chemin)
+test_chemin=test_chemin[keepTest]
+
+#je leur assigne leur label par le cluster entrainé
+train_arrive=clustering.predict(train_arrive)
 
 #dico taxiid vers embeddingid
 #j'ai testé: les taxis présents dans le test set sont présents dans le train
@@ -71,7 +69,6 @@ for i,tx in enumerate(idTaxi_train):
 dictClient={}
 idClient_train=np.unique(data_train['ORIGIN_CALL'])
 idClient_train=idClient_train[~np.isnan(idClient_train)]
-nbrClient=len(idClient_train)
 for i,cl in enumerate(idClient_train):
     if (int(cl) not in dictClient.keys()):
         dictClient[int(cl)]=i
@@ -79,10 +76,11 @@ for i,cl in enumerate(idClient_train):
 #c-a-d les personnes dont on ne connaît pas l'identité sont considérés comme la même personne
 numClNan=int(cl)+1
 dictClient[numClNan]=i+1
+nbrClient=len(dictClient)
 
 #la il faudrait prendre d'autres métadata mais j'ai pas fait encore
-metadata_train=littleTrain[['TAXI_ID','TIMESTAMP','ORIGIN_STAND']].copy()
-metadata_test=littleTest[['TAXI_ID','TIMESTAMP','ORIGIN_STAND']].copy()
+metadata_train=littleTrain[['TAXI_ID','TIMESTAMP','ORIGIN_STAND','DAY_TYPE']].copy()
+metadata_test=littleTest[['TAXI_ID','TIMESTAMP','ORIGIN_STAND','DAY_TYPE']].copy()
 #je supprime les données n'ayant pas passé le cut 
 x = metadata_train.values[keepTrain] 
 xtest=metadata_test.values[keepTest]
@@ -106,87 +104,104 @@ client_test_ids=np.asarray([dictClient.get(key) for key in client_test_ids])
 
 #il faudrait juste supprimer les lignes comme fait pour les metadonnees précédentes
 jourtype=np.array(['A','B','C'])
-daytyp_train=littleTrain['DAY_TYPE'].copy()
-daytyp_test=littleTest['DAY_TYPE'].copy()
+daytyp_train=pd.DataFrame(x[:,3])
+daytyp_test=pd.DataFrame(xtest[:,3])
 #je vais chercher leur id embeddings dans le dico
-daytyp_train=np.asarray([np.asscalar(np.where( jourtype == key)[0]) for key in daytyp_train])
-daytyp_test=np.asarray([np.asscalar(np.where( jourtype == key)[0]) for key in daytyp_test])
+daytyp_train=np.asarray([np.asscalar(np.where( jourtype == key)[0]) for key in np.asarray(daytyp_train)])
+daytyp_test=np.asarray([np.asscalar(np.where( jourtype == key)[0]) for key in np.asarray(daytyp_test)])
 
-#origin_train=x[:,2]
-#origin_test=xtest[:,2]
+#time
+dt_train= [datetime.fromtimestamp(stamp) for stamp in x[:,1]]
+week_train=np.asarray([w.date().isocalendar()[1] for w in dt_train])
+day_train=np.asarray([d.date().weekday() for d in dt_train])
+hour_train=np.asarray([h.hour for h in dt_train])
 
-#normalisation du temps
-time_train=x[:,1].reshape(-1,1)
-time_test=xtest[:,1].reshape(-1,1)
-min_max_scaler = preprocessing.MinMaxScaler()
-time_scaled = min_max_scaler.fit_transform(time_train)
-timetest_scaled=min_max_scaler.transform(time_test)
-#time_train= pd.DataFrame(time_scaled)
-#time_test= pd.DataFrame(timetest_scaled)
-
+dt_test = [datetime.fromtimestamp(stamp) for stamp in xtest[:,1]]
+week_test=np.asarray([w.date().isocalendar()[1] for w in dt_test])
+day_test=np.asarray([d.date().weekday() for d in dt_test])
+hour_test=np.asarray([h.hour for h in dt_test])
 
 
+nbrCluster=1000
+'''
 #le réseau temporaire
 class PredictionDest(nn.Module):
     
-    def __init__(self, nbrTaxi , dim_emb_tx=10):
+    def __init__(self, nbrCluster , nbrTaxi , nbrClient , nbrWeek=52 , nbrH=24 , nbrJ=7 , dim_emb_tx=10 , dim_emb_cl=10 , dim_emb_week=10 , dim_emb_heure=10 , dim_emb_jour=10):
         super(PredictionDest, self).__init__()
         self.embTaxiId=nn.Embedding(num_embeddings=nbrTaxi,embedding_dim=dim_emb_tx)
+        self.embClientId=nn.Embedding(num_embeddings=nbrClient,embedding_dim=dim_emb_cl)
+        self.embWeek=nn.Embedding(num_embeddings=nbrWeek,embedding_dim=dim_emb_week)
+        self.embHour=nn.Embedding(num_embeddings=nbrH,embedding_dim=dim_emb_heure)
+        self.embDay=nn.Embedding(num_embeddings=nbrJ,embedding_dim=dim_emb_jour)
         
         
-        self.lin1 = nn.Linear(in_features=31,out_features=1000)
-        self.lin2 = nn.Linear(1000,500)
-        self.lin3 = nn.Linear(500,2)
+        self.lin1 = nn.Linear(in_features=70,out_features=500)
+        self.lin2 = nn.Linear(500,nbrCluster)
         
-    def forward(self, path , taxi_ids, time):
+    def forward(self, path , taxi_ids , client_ids , week , day , hour):
         taxi_emb=self.embTaxiId(taxi_ids)
+        client_emb=self.embClientId(client_ids)
+        week_emb=self.embWeek(week)
+        day_emb=self.embDay(day)
+        hour_emb=self.embHour(hour)
         
-        x=torch.cat((taxi_emb,path,time), dim=1)
+        x=torch.cat((path , taxi_emb, client_emb , week_emb , day_emb , hour_emb), dim=1)
         
         x = self.lin1(x)
         x=F.relu(x)
         x = self.lin2(x)
-        x=F.relu(x)
-        x = self.lin3(x)
         return x
     
 
-def train(model, paths , taxi_ids , time , dest ,loss_fn , optimizer , use_cuda=False ,n_epochs=1,batch_size=32, verbose=True):
+def train(model, paths , taxi_ids , client_ids , weeks , days , hours , dest ,loss_fn , optimizer , use_cuda=False ,n_epochs=1,batch_size=32, verbose=True):
     
     model.train(True)
     
     loss_train = np.zeros(n_epochs)
     
     for epoch_num in range(n_epochs):
-        paths, taxi_ids, time , destinations = shuffle(paths,taxi_ids,time,dest)
+        paths, taxi_ids, client_ids , weeks , days , hours , destinations = shuffle(paths,taxi_ids,client_ids, weeks , days , hours ,dest)
 
         paths_tensor = gpu(torch.from_numpy(paths).type(torch.FloatTensor),
                               use_cuda)
         taxi_ids_tensor=gpu(torch.from_numpy(taxi_ids).type(torch.LongTensor),
                               use_cuda)
-        time_tensor=gpu(torch.from_numpy(time).type(torch.FloatTensor),
+        client_ids_tensor=gpu(torch.from_numpy(client_ids).type(torch.LongTensor),
                               use_cuda)
-        destinations_tensor = gpu(torch.from_numpy(destinations).type(torch.FloatTensor),
+        week_tensor=gpu(torch.from_numpy(weeks).type(torch.LongTensor),
+                              use_cuda)
+        day_tensor=gpu(torch.from_numpy(days).type(torch.LongTensor),
+                              use_cuda)
+        hour_tensor=gpu(torch.from_numpy(hours).type(torch.LongTensor),
+                              use_cuda)
+        destinations_tensor = gpu(torch.from_numpy(destinations).type(torch.LongTensor),
                              use_cuda)
         epoch_loss = 0.0
 
         for (minibatch_num,
              (batch_paths,
               batch_taxis,
-              batch_time,
+              batch_clients,
+              batch_week,
+              batch_day,
+              batch_hour,
               batch_destination)) in enumerate(minibatch(batch_size,
                                                      paths_tensor,
                                                      taxi_ids_tensor,
-                                                     time_tensor,
+                                                     client_ids_tensor,
+                                                     week_tensor,
+                                                     day_tensor,
+                                                     hour_tensor,
                                                      destinations_tensor)):
             
             
     
-            predictions = model(batch_paths, batch_taxis , batch_time)
+            predictions = model(batch_paths, batch_taxis , batch_clients , batch_week , batch_day , batch_hour)
 
             optimizer.zero_grad()
             
-            loss = loss_fn(batch_destination, predictions)
+            loss = loss_fn(predictions , batch_destination)
             
             epoch_loss = epoch_loss + loss.data.item()
             
@@ -206,43 +221,38 @@ def train(model, paths , taxi_ids , time , dest ,loss_fn , optimizer , use_cuda=
     
     return loss_train,predictions
 
-def test(model, paths , taxi_ids , time , loss_fn, destinations, use_cuda=False):
+def test(model, paths , taxi_ids , client_ids ,  weeks , days , hours , loss_fn, destinations, use_cuda=False):
     model.train(False)
 
     paths_tensor = gpu(torch.from_numpy(paths).type(torch.FloatTensor),
                               use_cuda)
     taxi_ids_tensor=gpu(torch.from_numpy(taxi_ids).type(torch.LongTensor),
                               use_cuda)
-    time_tensor=gpu(torch.from_numpy(time).type(torch.FloatTensor),
+    client_ids_tensor=gpu(torch.from_numpy(client_ids).type(torch.LongTensor),
                               use_cuda)
-    destinations_tensor = gpu(torch.from_numpy(destinations).type(torch.FloatTensor),
+    week_tensor=gpu(torch.from_numpy(weeks).type(torch.LongTensor),
+                          use_cuda)
+    day_tensor=gpu(torch.from_numpy(days).type(torch.LongTensor),
+                          use_cuda)
+    hour_tensor=gpu(torch.from_numpy(hours).type(torch.LongTensor),
+                              use_cuda)
+    destinations_tensor = gpu(torch.from_numpy(destinations).type(torch.LongTensor),
                              use_cuda)
-    predictions = model(paths_tensor, taxi_ids_tensor , time_tensor)
+    predictions = model(paths_tensor, taxi_ids_tensor , client_ids_tensor , week_tensor , day_tensor , hour_tensor)
         
     loss = loss_fn(destinations_tensor, predictions)
     
     return loss.data.item(),predictions
-
-
-#mes données d'entrainements
-destTr=train_arrive
-pathsTr=np.array([np.concatenate((train_chemin[i][:10],train_chemin[i][-10:])) for i in range(len(train_chemin))])
-
-#pathsTr=np.array([np.concatenate((x_scaled[i,:],train_chemin[i][:10],train_chemin[i][-10:])) for i in range(len(x_scaled))])
-
     
-taxi_class = PredictionDest(nbrTaxi)
+taxi_class = PredictionDest(nbrCluster=nbrCluster, nbrTaxi=nbrTaxi, nbrClient=nbrClient)
 use_gpu = torch.cuda.is_available()
 if use_gpu:
     taxi_class = taxi_class.cuda()
 
 #regarder le fichier torch_utils c'est la distance prise par kaggle
-loss_fn = Haversine_Loss
+#loss_fn = Haversine_Loss
+    
+loss_fn= nn.CrossEntropyLoss() 
 lr = 1e-3
 optimizer_cl = torch.optim.Adam(taxi_class.parameters())
-l_t,pred_tr= train(taxi_class, pathsTr , taxi_train_ids , time_scaled , destTr, loss_fn , optimizer_cl ,use_cuda=True, n_epochs = 10)
-
-
-#données de test
-pathsTe=np.array([np.concatenate((test_chemin[i][:10],test_chemin[i][-10:])) for i in range(len(test_chemin))])
-destTe=test_arrive
+l_t,pred_tr= train(taxi_class, train_chemin , taxi_train_ids , client_train_ids, week_train , day_train , hour_train , train_arrive , loss_fn , optimizer_cl ,use_cuda=True, n_epochs = 3)
